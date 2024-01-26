0b10285a570a26f540fe87b21076ba4197c98ec8
Jabid Sierra <jabidjunior@gmail.com>
2024-01-26 17:43:58 -0500

    first-commit

---
+++ src/routes/user.router.js
@@ -0,0 +1,16 @@
+const { getAll, create, getOne, remove, update } = require('../controllers/user.controler');
+const express = require('express');
+
+const userRouter = express.Router();
+
+userRouter.route("/users")
+		.get(getAll)
+        .post(create)
+
+userRouter.route("/users/:id")
+    .get(getOne)
+    .delete(remove)
+    .put(update)
+
+
+module.exports = userRouter;
